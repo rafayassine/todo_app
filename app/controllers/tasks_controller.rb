d6954579653433f19b83837578f6cb0efb53ae1c
Yassine Rafa <yassine.rafa@outlook.com>
2016-07-22 00:03:14 +0100

    Add all the action(index, show, new...) for the task controller and the index, new, show views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,41 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all.order("created_at DESC")
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:title, :description)
+    end
+
+    def find_task
+      @task = Task.find(params[:id])
+    end
 end
