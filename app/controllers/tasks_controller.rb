0f4ef1a29e6899def2011531c8cce3e7f8510d5b
Yassine Rafa <yassine.rafa@outlook.com>
2016-07-22 13:11:02 +0100

    Add styling and complete fonction on the index page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -39,7 +39,7 @@ class TasksController < ApplicationController
     redirect_to root_path
   end
   def complete
-    @item.update_attribue(:completed_at, Time.now)
+    @task.update_attribute(:completed_at, Time.now)
     redirect_to root_path
   end
 
